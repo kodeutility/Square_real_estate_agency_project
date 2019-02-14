8671c53d1887a2c666e8864ade9c4eea39f893f5
Kiran <kodeutility@gmail.com>
2019-02-14 20:51:59 +0530

    Project folder is created. Pages app is created and installed.

---
+++ real_estate_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\admin\desktop\square_real_estate_agency_project\real_estate_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
