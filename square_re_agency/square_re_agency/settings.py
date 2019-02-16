1c51c5c108d5dc3aed4a559aa1fec9a9b0d1e386
Kiran <kodeutility@gmail.com>
2019-02-17 00:03:37 +0530

    connected to PostgreSQL

--- square_re_agency/square_re_agency/settings.py
+++ square_re_agency/square_re_agency/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'square_re_agency.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'datbase',
+        'HOST': 'localhost',
     }
 }
 
