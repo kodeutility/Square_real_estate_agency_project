7df45a134a386387b582301f5457ac082e83a0cc
Kiran <kodeutility@gmail.com>
2019-02-18 22:26:42 +0530

    index.html and about.html is made dynamic with jinja2 template tags

--- square_re_agency/pages/views.py
+++ square_re_agency/pages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 def index(request):
-    return render(request,'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    return render(request,'pages/index.html',{'listings':listings})
 
 def about(request):
-    return render(request,'pages/about.html')
+    realtors = Realtor.objects.all()
+    mvp_realtors = Realtor.objects.filter(is_mvp=True)[:1]
+    return render(request,'pages/about.html',locals())
