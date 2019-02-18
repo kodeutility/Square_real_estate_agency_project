b52d6126620bf3816600e934cac887b1d02a3f9e
Kiran <kodeutility@gmail.com>
2019-02-18 21:01:31 +0530

    Pagination added to listings.html

--- square_re_agency/listings/views.py
+++ square_re_agency/listings/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from listings.models import Listing
 
 # Create your views here.
 def index(request):
-    listings = Listing.objects.all()
+    listings_list = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings_list, 2)
+
+    page = request.GET.get('page')
+    listings = paginator.get_page(page)
     return render(request,'listings/listings.html',{'listings':listings})
 
 def listing(request,listing_id):
